b06d1f27b3ea715bd0d054c4d6b5bfdb962a45bf
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-11 15:41:28 +0200

    Completed Task 7 & 8

--- func.py
+++ func.py
@@ -1,5 +1,15 @@
+# Tasks 7 & 8 are the same
+
+from assets.array import text
+
 def main():
     try:
-        pass
+        _write()
+        print('Task is completed!')
     except Exception as e:
         print(f'Error: {e}')
+
+def _write():
+    with open('assets/processed_file.txt', 'w') as write_:
+        for line in text:
+            write_.write(line + '\n')
