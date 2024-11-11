d8a8e4e1c5a175dff1483a85a516f84df88cb74a
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-11 15:33:17 +0200

    Completed Task 6

--- func.py
+++ func.py
@@ -1,5 +1,17 @@
 def main():
     try:
-        pass
+        _write()
+        print('Task is completed!')
     except Exception as e:
         print(f'Error: {e}')
+
+def _write():
+    with open('assets/original_file.txt', 'r') as read_:
+        with open('assets/processed_file.txt', 'w') as write_:
+            read_ = read_.readlines()
+
+            for number_of_line in range(len(read_)):
+                line = (read_[number_of_line].replace('\n', ''))
+                line = line.replace('*', '𝓽').replace('&', '*').replace('𝓽', '&')
+
+                write_.write(line + '\n')
