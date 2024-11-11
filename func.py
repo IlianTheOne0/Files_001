7acef47060205a67aeae56b03dbde5903deae6e1
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-11 15:05:09 +0200

    Completed Task 5

--- func.py
+++ func.py
@@ -1,5 +1,21 @@
 def main():
     try:
-        pass
+        symbol = str(input('Enter a character to start counting words: '))
+        print(f'{_write(symbol)} words were found after {symbol} in the file')
     except Exception as e:
         print(f'Error: {e}')
+
+def _write(symbol: str) -> int:
+    with open('assets/original_file.txt', 'r') as read_:
+        read_ = read_.readlines()
+
+        counter = 0
+
+        for line in read_:
+            line = line.split()
+
+            for word in line:
+                if symbol in word:
+                    counter += 1
+
+    return counter
