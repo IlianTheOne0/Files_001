6556dba0358aec6461b2e9c8a291602d42aeae7f
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-11 23:16:29 +0200

    Completed Task 4

--- func.py
+++ func.py
@@ -1,5 +1,21 @@
 def main():
     try:
-        pass
+        biggest_line, line_number = function()
+        print(f'The biggest line is: {biggest_line} (line {line_number})')
     except Exception as e:
         print(f'Error: {e}')
+
+def function():
+    with open('assets/original_file.txt', 'r') as read_:
+        with open('assets/processed_file.txt', 'w') as write_:
+            read_ = read_.readlines()
+
+            the_biggest, number = read_[0], 1
+
+            for number_of_line in range(1, len(read_)):
+                line = (read_[number_of_line].replace('\n', ''))
+
+                if len(line) > len(the_biggest):
+                    the_biggest, number = line, number_of_line + 1
+
+    return the_biggest, number
