0414e4fa4fe37b823128322fe2792d6bcffa1a33
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-11-11 15:46:22 +0200

    Completed Task 9

--- func.py
+++ func.py
@@ -1,5 +1,19 @@
 def main():
     try:
-        pass
+        print('Chars is the text:', _write())
     except Exception as e:
         print(f'Error: {e}')
+
+def _write() -> int:
+    with open('assets/original_file.txt', 'r') as read_:
+        read_ = read_.readlines()
+
+        counter = 0
+
+        for number_of_line in range(0, len(read_)):
+            line = (read_[number_of_line].replace('\n', ''))
+
+            for temp in line:
+                counter += 1
+
+    return counter
